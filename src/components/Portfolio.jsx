a3581c594f5ef9d53cdb880e2ca630d76826abbc
Eternum101 <eternumhd111@gmail.com>
2024-03-20 18:28:04 +1000

    Added Images for Portfolio Section in Assets Folder Added Images in Portfolio.jsx Modified Styling in Portfolio.css

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+import SocialMediaMockup from '../assets/social-media-mockup.png';
+import MessagingAppMockup from '../assets/messaging-app-mockup.png';
+import PhotoTagMockup from '../assets/photo-tag-mockup.png';
+import WeatherAppMockup from '../assets/weather-app-mockup.png';
+import ShoppingCartMockup from '../assets/shopping-cart-mockup.png';
+import TicTacToeMockup from '../assets/tic-tac-toe-mockup.png';
 
 export function Portfolio() {
     return (
@@ -17,65 +23,77 @@ export function Portfolio() {
         <div className="portfolio-cards-container">
           <div className="portfolio-row">
             <div className="portfolio-card">
-              <img className="portfolio-image" alt="Image" src="image.png" />
+              <img className="portfolio-image" alt="Social Media Application" src={SocialMediaMockup} />
               <div className="portfolio-card-desc-container">
                 <div className="portfolio-card-desc">
                   <div className="portfolio-card-title">Social Media Application</div>
                   <p className="portfolio-card-sub-title">Web Design / Development</p>
                 </div>
+              <a href="https://social-media-app-el95.onrender.com/">
                 <button className="portfolio-icon"><BsFillArrowUpRightCircleFill /></button>
+              </a>
               </div>
             </div>
             <div className="portfolio-card">
-              <img className="portfolio-image" alt="Image" src="image.svg" />
+              <img className="portfolio-image" alt="Messaging Application" src={MessagingAppMockup} />
               <div className="portfolio-card-desc-container">
                 <div className="portfolio-card-desc">
                   <div className="portfolio-card-title">Messaging Application</div>
                   <p className="portfolio-card-sub-title">Web Design / Development</p>
                 </div>
+              <a href="https://messaging-app-2mr0.onrender.com/">
                 <button className="portfolio-icon"><BsFillArrowUpRightCircleFill /></button>
+              </a>
               </div>
             </div>
             <div className="portfolio-card">
-              <img className="portfolio-image" alt="Image" src="image-2.png" />
+              <img className="portfolio-image" alt="Photo Tagging Application" src={PhotoTagMockup} />
               <div className="portfolio-card-desc-container">
                 <div className="portfolio-card-desc">
                   <div className="portfolio-card-title">Photo Tagging Application</div>
                   <div className="portfolio-card-sub-title">Web Design / Development</div>
                 </div>
+              <a href="https://photo-tagging-app-lzga.onrender.com/">
                 <button className="portfolio-icon"><BsFillArrowUpRightCircleFill /></button>
+              </a>
               </div>
             </div>
           </div>
           <div className="portfolio-row">
             <div className="portfolio-card">
-              <img className="portfolio-image" alt="Image" src="image-2.svg" />
+              <img className="portfolio-image" alt="Weather Application" src={WeatherAppMockup} />
               <div className="portfolio-card-desc-container">
                 <div className="portfolio-card-desc">
                   <div className="portfolio-card-title">Weather Application</div>
                   <div className="portfolio-card-sub-title">Web Design / Development</div>
                 </div>
+              <a href="https://eternum101.github.io/weather-app/">
                 <button className="portfolio-icon"><BsFillArrowUpRightCircleFill /></button>
+              </a>
               </div>
             </div>
             <div className="portfolio-card">
-              <img className="portfolio-image" alt="Image" src="image-3.png" />
+              <img className="portfolio-image" alt="Shopping Cart Project" src={ShoppingCartMockup} />
               <div className="portfolio-card-desc-container">
                 <div className="portfolio-card-desc">
                   <div className="portfolio-card-title">Shopping Cart Project</div>
                   <div className="portfolio-card-sub-title">Web Design / Development</div>
                 </div>
+              <a href="https://brayden-shopping-cart.netlify.app/">
                 <button className="portfolio-icon"><BsFillArrowUpRightCircleFill /></button>
+              </a>
               </div>
             </div>
             <div className="portfolio-card">
-              <img className="portfolio-image" alt="Image" src="image-4.png" />
+              <img className="portfolio-image" alt="Tic Tac Toe Game" src={TicTacToeMockup} />
               <div className="portfolio-card-desc-container">
                 <div className="portfolio-card-desc">
                   <div className="portfolio-card-title">Tic Tac Toe Game</div>
                   <div className="portfolio-card-sub-title">Web Design / Development</div>
                 </div>
+              <a href="https://eternum101.github.io/tic-tac-toe/">
                 <button className="portfolio-icon"><BsFillArrowUpRightCircleFill /></button>
+              </a>
               </div>
             </div>
           </div>
